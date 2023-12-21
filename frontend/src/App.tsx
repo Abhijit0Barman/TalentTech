48915d767df8ee69f04b375d5b6678ff22880fef
Abhijit Barman <113384779+Abhijit0Barman@users.noreply.github.com>
2023-12-22 02:01:44 +0530

    gg

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-lg font-bold underline">
+     <h1>Abhijt Barman</h1>
+    </div>
+  );
+}
+
+export default App;
